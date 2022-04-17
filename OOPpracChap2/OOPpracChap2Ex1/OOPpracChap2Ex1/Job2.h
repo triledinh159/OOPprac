d5cf09f33de6a156c9413a41eb0b1ce2e6049e03
triledinh159 <91708260+triledinh159@users.noreply.github.com>
2022-04-17 20:17:36 +0700

    Add files via upload

---
+++ OOPpracChap2/OOPpracChap2Ex1/OOPpracChap2Ex1/Job2.h
@@ -0,0 +1,66 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class STUDENT {
+	int ID;
+	char CLASS[100];
+	char TELENUM[100];
+	char SCHOOL[100];
+	char FACULTY[100];
+public:
+	void inputStudent();
+};
+class PUPIL  {
+	int ID;
+	char CLASS[100];
+	char TELENUM[100];
+	char SCHOOL[100];
+public:
+	void inputPupil();
+	
+};
+class WORKER  {
+	char WORKPLACE[100];
+public:
+	int SALARY;
+	void inputWorker();
+	
+};
+class ACTOR  {
+	char ARTKIND[100];
+	int WORKAGE;
+public:
+	int SALARY;
+	void inputActor();
+	
+};
+class SINGER  {
+	int WORKAGE;
+	char MUSICKIND[100];
+	int SOLDDISK;
+	int SHOW;
+public:
+	int SALARY;
+	void inputSinger();
+	bool FAMOUS();
+	
+};
+class PEOPLE :public STUDENT,public WORKER,public PUPIL,public ACTOR,public SINGER{
+	char NAME[20];
+	int ID;
+	int DAY;
+	int MONTH;
+	int YEAR;
+	char HOMETOWN[20];
+	int ROLE;
+public:
+	static int COUNT;
+	void INPUT();
+	int ROLES();
+	
+	void findTheHigestSalary(PEOPLE listA, int n, int& count, int& max, int& maxSinger, string& nameSinger, string &name);
+	static int getCount() {
+		return COUNT;
+	}
+};
+int PEOPLE::COUNT = 0;
